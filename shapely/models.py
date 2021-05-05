78e0c2afe8fda7ab0241cdaedc5ba95f27135562
mintemat000 <mattminter@outlook.com>
2021-05-05 13:52:20 -0500

    initial commit

---
+++ shapely/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Destination(models.Model):
+
+    name : str
+    img : str
+    desc : str
+    price : int
+    offer : bool=False
+
+
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics', height_field=None, width_field=None, max_length=None)
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+
+
